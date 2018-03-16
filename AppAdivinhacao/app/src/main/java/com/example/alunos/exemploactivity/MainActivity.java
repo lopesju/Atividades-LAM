67a69c93743ac725a6b24bcbfed347afd7296700
lopesju <julialopesoliviercefet@gmail.com>
2018-03-16 10:16:02 -0300

    fiz o app

--- AppAdivinhacao/app/src/main/java/com/example/alunos/exemploactivity/MainActivity.java
+++ AppAdivinhacao/app/src/main/java/com/example/alunos/exemploactivity/MainActivity.java
@@ -12,35 +12,33 @@ public class MainActivity extends AppCompatActivity {
 
     int nSort;
     int t;
+    boolean achou;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Random r = new Random();
         t = 3;
         nSort = r.nextInt(10) + 1;
+
     }
     public void verificaTexto(View v){
         EditText userInput = findViewById(R.id.numero);
         String num = userInput.getText().toString();
             int nDigitado = Integer.parseInt(num);
             TextView resposta = findViewById(R.id.titulo);
-            TextView resultado = findViewById(R.id.resultado);
 
+            if(nSort==nDigitado && t>0){
+                resposta.setText(getResources().getString(R.string.str_rsp1,nSort).toString());
+                userInput.setText("");
 
-        if(nSort==nDigitado && t>0){
-            resposta.setText(getResources().getString(R.string.str_rsp1).toString());
-            userInput.setText("");
-            String nSorteado = Integer.toString(nSort);
-            resultado.setText(getResources().getString(R.string.str_rsp3,nSorteado).toString());
-
-        }else if(nSort!=nDigitado && t>0) {
-            resposta.setText(getResources().getString(R.string.str_rsp2).toString());
-            userInput.setText("");
-            t--;
-        }else{
-        System.out.printf("O número era: %d \n", nSort);
-        }
-
+            }else if(nSort!=nDigitado && t>0){
+                t--;
+                resposta.setText(getResources().getString(R.string.str_rsp2,t).toString());
+                userInput.setText("");
+            }else{
+                resposta.setText(getResources().getString(R.string.str_rsp3,nSort).toString());
+            }
     }
+
 }
 
